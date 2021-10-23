096c73ea7a9b0183ad6825cefe2c9bd086430739
Oluwafemi <osshobey@gmail.com>
2021-10-23 14:33:25 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.31"
+}
+
+group = "org.volunteered"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
