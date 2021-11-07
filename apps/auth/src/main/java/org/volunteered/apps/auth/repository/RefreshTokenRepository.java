4e57b7c6ec7ed83889ff4333dc57b26e29f01e4e
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2021-11-07 22:43:04 +0100

    refactoring RefreshTokenRepository interface
    Signed-off-by: ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>

--- apps/auth/src/main/java/org/volunteered/apps/auth/repository/RefreshTokenRepository.java
+++ apps/auth/src/main/java/org/volunteered/apps/auth/repository/RefreshTokenRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
-    Optional<RefreshToken> findById(Long id);
-
     Optional<RefreshToken> findByToken(String token);
 }
