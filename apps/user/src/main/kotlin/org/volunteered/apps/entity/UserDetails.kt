b6e4fd32bce3e409de4bf9a375894e2d8e6c9491
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2021-11-20 17:22:27 +0100

    CU-1r51wy7 volunteered signup, some cleanup

--- apps/user/src/main/kotlin/org/volunteered/apps/entity/UserDetails.kt
+++ apps/user/src/main/kotlin/org/volunteered/apps/entity/UserDetails.kt
@@ -1,16 +1,78 @@
 package org.volunteered.apps.entity
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.*
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 @Entity
 @Table(name = "user")
 class UserDetails (
-    @Id @GeneratedValue var id: Long? = null,
-
+    @Id @GeneratedValue var id: Long,
     @NotNull var firstName : String,
     @NotNull var lastName : String,
     @NotNull var phone : String,
     @Email var email : String,
-    var country : String)
+    var country : String,
+    var city : String,
+    @Size(max = 2000) var bio : String,
+    @Enumerated var gender : Gender,
+
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinTable(name = "user_main_skill")
+    var mainSkills : Set<Skill> = HashSet(),
+
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinTable(name = "user_other_skill")
+    var otherSkills : Set<Skill> = HashSet(),
+
+    @JsonIgnore
+    @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinTable(name = "user_language")
+    var spokenLanguages : Set<Language> = HashSet(),
+
+    @Column(name = "resume_url") var resumeUrl : String,
+    @Column(name = "portfolio_url")var portfolioUrl : String,
+    @Column(name = "profile_photo_url") var profilePhotoUrl : String,
+    @OneToOne var userSocialMedia: UserSocialMedia
+    )
+
+
+
+
+@Entity
+@Table(name = "user_social_media")
+class UserSocialMedia (
+    @Id @GeneratedValue var id: Long,
+    var linkedinUrl : String? = null,
+    var facebookUrl : String? = null,
+    var twitterUrl : String? = null,
+    var skypeUrl : String? = null,
+    var websiteUrl : String? = null,
+)
+
+
+
+@Entity
+@Table(name = "skill")
+class Skill (
+    @Id @GeneratedValue var id: Long,
+    @NotNull var name : String,
+)
+
+
+
+@Entity
+@Table(name = "language")
+class Language (
+    @Id @GeneratedValue var id: Long,
+    @NotNull var name : String,
+)
+
+
+enum class Gender{
+    MALE, FEMALE, NON_BINARY
+}
--- apps/user/src/main/kotlin/org/volunteered/apps/service/UserService.kt
+++ apps/user/src/main/kotlin/org/volunteered/apps/service/UserService.kt
@@ -1,8 +1,13 @@
 package org.volunteered.apps.service
 
 import net.devh.boot.grpc.server.service.GrpcService
+import org.volunteered.apps.entity.Gender
+import org.volunteered.apps.entity.UserSocialMedia
 import org.volunteered.libs.common.v1.User
+import org.volunteered.libs.common.v1.user
+import org.volunteered.libs.common.v1.userSocialMedia
 import org.volunteered.libs.user.v1.CreateUserRequest
+import org.volunteered.libs.user.v1.GetRequest
 import org.volunteered.libs.user.v1.UpdateUserRequest
 import org.volunteered.libs.user.v1.UserServiceGrpcKt
 
@@ -13,38 +18,94 @@ class UserService(private val userDetailService: UserDetailService ): UserServic
         val email = request.email
         val userDetails = userDetailService.findByEmail(email)
 
-        return User.newBuilder()
-            .setEmail(userDetails?.email)
-            .setFirstName(userDetails?.firstName)
-            .setLastName(userDetails?.lastName)
-            .setPhone(userDetails?.phone)
-            .setEmail(userDetails?.email)
-            .setCountry(userDetails?.country)
-            .build()
-
+        return user {
+            userDetails?.email;
+            userDetails?.firstName;
+            userDetails?.lastName;
+            userDetails?.phone;
+            userDetails?.email;
+            userDetails?.country
+            userDetails?.city
+            userDetails?.bio
+            userDetails?.gender
+            userDetails?.resumeUrl
+            userDetails?.portfolioUrl
+            userDetails?.profilePhotoUrl
+            userDetails?.userSocialMedia
+            userDetails?.mainSkills
+            userDetails?.otherSkills
+            userDetails?.spokenLanguages
+        }
     }
 
     override suspend fun update(request: UpdateUserRequest): User {
 
-        val id = request.id
+        val email = request.email
         val user = request.user;
-        var userDetails = userDetailService.findByEmail(id.value)
+        var userDetails = userDetailService.findByEmail(email.value)
 
         userDetails?.phone = user.phone
         userDetails?.email = user.email
         userDetails?.firstName = user.firstName
         userDetails?.lastName = user.lastName
+        userDetails?.phone = user.phone
+        userDetails?.email = user.email
+        userDetails?.country = user.country
+        userDetails?.city = user.city
+        userDetails?.bio = user.bio
+        userDetails?.gender = Gender.valueOf(user.gender.name)
+        userDetails?.resumeUrl = user.resumeUrl
+        userDetails?.portfolioUrl = user.portfolioUrl
+        userDetails?.profilePhotoUrl = user.profilePhotoUrl
 
         userDetails = userDetails?.let { userDetailService.save(it) }
 
-        return User.newBuilder()
-            .setEmail(userDetails?.email)
-            .setFirstName(userDetails?.firstName)
-            .setLastName(userDetails?.lastName)
-            .setPhone(userDetails?.phone)
-            .setEmail(userDetails?.email)
-            .setCountry(userDetails?.country)
-            .build()
+        return user {
+            userDetails?.email;
+            userDetails?.firstName;
+            userDetails?.lastName;
+            userDetails?.phone;
+            userDetails?.email;
+            userDetails?.country
+            userDetails?.city
+            userDetails?.bio
+            userDetails?.gender
+            userDetails?.resumeUrl
+            userDetails?.portfolioUrl
+            userDetails?.profilePhotoUrl
+            userDetails?.userSocialMedia
+            userDetails?.mainSkills
+            userDetails?.otherSkills
+            userDetails?.spokenLanguages
+            userDetails?.userSocialMedia
+        }
 
     }
-}
+
+    override suspend fun get(request: GetRequest): User {
+        val email = request.email
+        val userDetails = userDetailService.findByEmail(email)
+
+        return user {
+            userDetails?.email;
+            userDetails?.firstName;
+            userDetails?.lastName;
+            userDetails?.phone;
+            userDetails?.email;
+            userDetails?.country
+            userDetails?.city
+            userDetails?.bio
+            userDetails?.gender
+            userDetails?.resumeUrl
+            userDetails?.portfolioUrl
+            userDetails?.profilePhotoUrl
+            userDetails?.userSocialMedia
+            userDetails?.mainSkills
+            userDetails?.otherSkills
+            userDetails?.spokenLanguages
+            userDetails?.userSocialMedia
+        }
+
+    }
+
+    }
