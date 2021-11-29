2bc78c7258610558fe88c04cc829c8785bf3e218
Oluwafemi <osshobey@gmail.com>
2021-11-29 14:29:15 +0200

    CU-1t7d06w create organization implementation

--- apps/organization/src/main/kotlin/org/volunteered/apps/service/impl/OrganizationServiceImpl.kt
+++ apps/organization/src/main/kotlin/org/volunteered/apps/service/impl/OrganizationServiceImpl.kt
@@ -3,9 +3,11 @@ package org.volunteered.apps.service.impl
 import com.google.protobuf.Empty
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.volunteered.apps.exception.CreatorDoesNotExistException
 import org.volunteered.apps.exception.OrganizationAlreadyExistsException
 import org.volunteered.apps.exception.OrganizationDoesNotExistException
 import org.volunteered.apps.repository.OrganizationRepository
+import org.volunteered.apps.repository.OrganizationSubsidiaryRepository
 import org.volunteered.apps.service.OrganizationService
 import org.volunteered.apps.util.DtoTransformer
 import org.volunteered.libs.organization.v1.CreateOrganizationRequest
@@ -14,23 +16,28 @@ import org.volunteered.libs.organization.v1.DeleteOrganizationRequest
 import org.volunteered.libs.organization.v1.GetOrganizationRequest
 import org.volunteered.libs.proto.common.v1.Organization
 import org.volunteered.libs.proto.common.v1.OrganizationSubsidiary
+import org.volunteered.libs.user.v1.UserServiceGrpcKt
+import org.volunteered.libs.user.v1.existsByIdRequest
 
 @Service
 class OrganizationServiceImpl(
     private val organizationRepository: OrganizationRepository,
-//    @GrpcClient("userService") private val userServiceStub: UserServiceGrpcKt.UserServiceCoroutineStub
+    private val organizationSubsidiaryRepository: OrganizationSubsidiaryRepository,
+    @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+    private val userServiceStub: UserServiceGrpcKt.UserServiceCoroutineStub,
 ) : OrganizationService {
     override suspend fun createOrganization(request: CreateOrganizationRequest): Organization {
-//        val user = userServiceStub.getUserById(
-//            getUserByIdRequest {
-//                id = request.creatorId
-//            }
-//        )
+        val creatorExists = userServiceStub.existsById(
+            existsByIdRequest { id = request.creatorId }
+        ).value
+        if (!creatorExists)
+            throw CreatorDoesNotExistException("Creator does not exist")
 
         if (organizationRepository.existsByEmail(request.email))
             throw OrganizationAlreadyExistsException("Organization already exists")
 
         val organizationEntity = DtoTransformer.transformCreateOrganizationRequestToOrganizationEntity(request)
+        organizationSubsidiaryRepository.save(organizationEntity.hq)
         val createdOrganizationEntity = organizationRepository.save(organizationEntity)
 
         return DtoTransformer.transformOrganizationEntityToOrganizationDto(createdOrganizationEntity)
