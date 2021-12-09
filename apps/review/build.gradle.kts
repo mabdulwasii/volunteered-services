29e241a431c081a222e4d5c7f2acf91cbc7d0009
Oluwafemi <osshobey@gmail.com>
2021-12-09 16:34:54 +0200

    CU-1r53dp3 review service skeleton

---
+++ apps/review/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+    kotlin("plugin.allopen")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.Embeddable")
+    annotation("javax.persistence.MappedSuperclass")
+}
+
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation(project(":libs:core"))
+    implementation(project(":libs:grpc"))
+    implementation(project(":libs:proto"))
+
+    implementation(libs.bundles.spring.grpc)
+
+    implementation(libs.flyway.core)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.javax.validation)
+
+    runtimeOnly(libs.h2)
+
+    testImplementation(libs.spring.boot.starter.test)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
