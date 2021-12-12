bfcee692b14c1bc71e0327cd3c7765dab341ba50
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2021-12-12 14:25:58 +0100

    CU-1nh9mbx Add get organization reviews

--- apps/review/src/main/kotlin/org/volunteered/apps/service/impl/ReviewServiceImpl.kt
+++ apps/review/src/main/kotlin/org/volunteered/apps/service/impl/ReviewServiceImpl.kt
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Service
 import org.volunteered.apps.repository.ReviewRepository
 import org.volunteered.apps.service.ReviewService
 import org.volunteered.apps.util.DtoTransformer
+import org.volunteered.libs.proto.common.v1.Organization
 import org.volunteered.libs.proto.common.v1.OrganizationSubsidiary
 import org.volunteered.libs.proto.common.v1.User
-import org.volunteered.libs.proto.common.v1.paginationResponse
 import org.volunteered.libs.proto.organization.v1.OrganizationServiceGrpcKt
+import org.volunteered.libs.proto.organization.v1.getOrganizationRequest
 import org.volunteered.libs.proto.organization.v1.getOrganizationSubsidiaryRequest
 import org.volunteered.libs.proto.review.v1.DeleteReviewRequest
 import org.volunteered.libs.proto.review.v1.GetOrganizationReviewsRequest
@@ -20,7 +21,6 @@ import org.volunteered.libs.proto.review.v1.ReplyReviewRequest
 import org.volunteered.libs.proto.review.v1.Review
 import org.volunteered.libs.proto.review.v1.ReviewReply
 import org.volunteered.libs.proto.review.v1.WriteReviewRequest
-import org.volunteered.libs.proto.review.v1.getReviewsResponse
 import org.volunteered.libs.proto.user.v1.UserServiceGrpcKt
 import org.volunteered.libs.proto.user.v1.getUserByIdRequest
 
@@ -43,14 +43,16 @@ class ReviewServiceImpl(
     }
 
     override suspend fun getOrganizationReviews(request: GetOrganizationReviewsRequest): GetReviewsResponse {
-//        val organizationId = request.organizationId
-//        val pagination = request.pagination
-//        val limitPerPage = pagination.limitPerPage
-//        pagination.page
-//
-//        val findAllByOrOrganizationSubsidiaryId =
-//            reviewRepository.findAllByOrOrganizationSubsidiaryId(organizationId)
-        TODO("Not yet implemented")
+        val organizationId = request.organizationId
+        val pagination = request.pagination
+        val paginationLimitPerPage = pagination.limitPerPage
+        val paginationPage = pagination.page
+
+        val pageable = PageRequest.of(paginationPage, paginationLimitPerPage)
+
+        val retrievedReviews = reviewRepository.findAllByOrganizationId(organizationId, pageable)
+
+        return DtoTransformer.transformReviewEntityListToReviewDtoList(retrievedReviews, pagination)
     }
 
     override suspend fun getOrganizationSubsidiaryReviews(request: GetOrganizationSubsidiaryReviewsRequest): GetReviewsResponse {
@@ -64,16 +66,7 @@ class ReviewServiceImpl(
         val retrievedReviews =
             reviewRepository.findAllByOrganizationSubsidiaryId(organizationSubsidiaryId, pageable)
 
-        val paginationResponse = paginationResponse {
-            total = retrievedReviews.totalElements
-            limitPerPage = paginationLimitPerPage
-            page = paginationPage
-        }
-        val reviewsDtoList = DtoTransformer.transformReviewEntityListToReviewDtoList(retrievedReviews.content)
-        return getReviewsResponse {
-            this.reviews.addAll(reviewsDtoList)
-            this.pagination = paginationResponse
-        }
+        return DtoTransformer.transformReviewEntityListToReviewDtoList(retrievedReviews, pagination)
     }
 
     override suspend fun markReviewAsHelpful(request: MarkReviewAsHelpfulRequest): Empty {
@@ -97,6 +90,13 @@ class ReviewServiceImpl(
     private suspend fun getOrganizationSubsidiaryById(organizationSubsidiaryId: Long): OrganizationSubsidiary {
         return organizationServiceStub.getOrganizationSubsidiaryById(
             getOrganizationSubsidiaryRequest { id = organizationSubsidiaryId }
+        )
+    }
+
+    private suspend fun getOrganizationSubsidiaryByOrganizationId(organizationId: Long):
+            Organization {
+        return organizationServiceStub.getOrganizationById(
+            getOrganizationRequest { id = organizationId }
         )
     }
 }
