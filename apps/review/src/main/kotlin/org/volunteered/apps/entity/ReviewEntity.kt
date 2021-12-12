162bb4e0822191a4bd7a75974586e22b02868adf
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2021-12-12 13:01:29 +0100

    CU-1nh9mbx get organization subsidiary reviews

--- apps/review/src/main/kotlin/org/volunteered/apps/entity/ReviewEntity.kt
+++ apps/review/src/main/kotlin/org/volunteered/apps/entity/ReviewEntity.kt
@@ -15,21 +15,27 @@ class ReviewEntity(
     @Column(name = "id", nullable = false)
     var id: Long? = null,
 
+    @Column(name = "user_id")
+    var userId : Long,
+
+    @Column(name = "organization_subsidiary_id")
+    var organizationSubsidiaryId  : Long,
+
     @Column(name = "user_display_name")
     var userDisplayName : String,
 
     @Column(name = "user_avatar")
-    var userAvatar : String,
+    var userAvatar : String? = null,
 
     var rating : Int,
 
     var body : String,
 
     @Column(name = "helpful_count")
-    var helpfulCount : Long,
+    var helpfulCount : Long ? = null,
 
     @Column(name = "organization_subsidiary_city")
     var organizationSubsidiaryCity : String,
 
-    var verified : Boolean
+    var verified : Boolean? = false
 )
