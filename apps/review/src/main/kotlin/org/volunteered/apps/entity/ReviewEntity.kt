2938f156e5831151f64379c674910391ab5d872d
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-01-17 18:00:08 +0100

    code cleanup review entity

--- apps/review/src/main/kotlin/org/volunteered/apps/entity/ReviewEntity.kt
+++ apps/review/src/main/kotlin/org/volunteered/apps/entity/ReviewEntity.kt
@@ -19,23 +19,11 @@ class ReviewEntity(
 
     var body : String? = null,
 
-    @Column(name = "organization_id")
-    var organizationId  : Long,
-
-    @Column(name = "organization_subsidiary_city")
-    var organizationSubsidiaryCity : String,
-
     @Column(name = "user_id")
-    var userId : String? = null,
+    var userId : String,
 
     @Column(name = "organization_subsidiary_id")
     var organizationSubsidiaryId  : Long,
-
-    @Column(name = "user_display_name")
-    var userDisplayName : String? = null,
-
-    @Column(name = "user_avatar")
-    var userAvatar : String? = null,
 
     @Column(name = "helpful_count")
     var helpfulCount : Long ?= null,
