1cf93c05c32e07ab996283c5457306c42b8821bd
ABDULWASIU-MORONKOLA <moronkola@systemspecs.com.ng>
2022-01-17 19:53:09 +0100

    create and inject RatingConfigProperties into ratingCalculator

--- apps/review/src/main/kotlin/org/volunteered/apps/review/ReviewApplication.kt
+++ apps/review/src/main/kotlin/org/volunteered/apps/review/ReviewApplication.kt
@@ -1,11 +1,12 @@
 package org.volunteered.apps.review
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.volunteered.apps.review.config.RatingConfigProperties
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
+@EnableConfigurationProperties(RatingConfigProperties::class)
 class ReviewApplication
 
 fun main(args: Array<String>) {
--- apps/review/src/main/kotlin/org/volunteered/apps/review/util/RatingCalculator.kt
+++ apps/review/src/main/kotlin/org/volunteered/apps/review/util/RatingCalculator.kt
@@ -1,33 +1,35 @@
 package org.volunteered.apps.review.util
 
+import org.springframework.context.annotation.Configuration
+import org.volunteered.apps.review.config.RatingConfigProperties
 import org.volunteered.apps.review.entity.RatingEntity
 
-class RatingCalculator {
-    companion object {
-        // TODO get these values from config. Create a RatingConfig class to get the config values and inject that
-        //  class into this one on instantiation. Let an instance of this class be passed into ReviewServiceImpl
-        //  Hence, the subsequent functions need not be encapsulated in a companion object
-        private val UNVERIFIED_RATING_WEIGHT = 1
-        private val VERIFIED_RATING_WEIGHT = 3
+@Configuration
+class RatingCalculator(private val ratingConfigProperties: RatingConfigProperties) {
+    // TODO get these values from config. Create a RatingConfig class to get the config values and inject that
+    //  class into this one on instantiation. Let an instance of this class be passed into ReviewServiceImpl
+    //  Hence, the subsequent functions need not be encapsulated in a companion object
+    private val UNVERIFIED_RATING_WEIGHT = ratingConfigProperties.unverifiedRating
+    private val VERIFIED_RATING_WEIGHT = ratingConfigProperties.verifiedRatingWeight
 
-        fun recomputeOrganizationSubsidiaryRating(
-            orgSubsidiaryRating: RatingEntity,
-            newRating: Int,
-            isNewRatingVerified: Boolean
-        ): Int {
-            val newRatingWeight = if (isNewRatingVerified) VERIFIED_RATING_WEIGHT else UNVERIFIED_RATING_WEIGHT
+    fun recomputeOrganizationSubsidiaryRating(
+        orgSubsidiaryRating: RatingEntity,
+        newRating: Int,
+        isNewRatingVerified: Boolean
+    ): Int {
+        val newRatingWeight = if (isNewRatingVerified) VERIFIED_RATING_WEIGHT else UNVERIFIED_RATING_WEIGHT
 
-            val numerator =
-                (orgSubsidiaryRating.unverifiedRatingCount * UNVERIFIED_RATING_WEIGHT * orgSubsidiaryRating.rating)
-                    .plus(orgSubsidiaryRating.verifiedRatingCount * VERIFIED_RATING_WEIGHT * orgSubsidiaryRating.rating)
-                    .plus(newRating * newRatingWeight)
+        val numerator =
+            (orgSubsidiaryRating.unverifiedRatingCount * UNVERIFIED_RATING_WEIGHT * orgSubsidiaryRating.rating)
+                .plus(orgSubsidiaryRating.verifiedRatingCount * VERIFIED_RATING_WEIGHT * orgSubsidiaryRating.rating)
+                .plus(newRating * newRatingWeight)
 
-            val denominator = (orgSubsidiaryRating.unverifiedRatingCount * UNVERIFIED_RATING_WEIGHT)
-                .plus(orgSubsidiaryRating.verifiedRatingCount * VERIFIED_RATING_WEIGHT)
-                .plus(1)
+        val denominator = (orgSubsidiaryRating.unverifiedRatingCount * UNVERIFIED_RATING_WEIGHT)
+            .plus(orgSubsidiaryRating.verifiedRatingCount * VERIFIED_RATING_WEIGHT)
+            .plus(1)
 
-            // TODO should not be integer division
-            return numerator / denominator
-        }
+        // TODO should not be integer division
+        return numerator / denominator
     }
+
 }
